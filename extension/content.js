c1e0bf7beb79d0fd1500876cc98388466c633f91
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-01-10 18:04:43 +0100

    Improvements for Firefox

--- extension/content.js
+++ extension/content.js
@@ -1,32 +1,32 @@
-// Si premier lancement, active isOn
+// Si premier lancement, ajoute isOn
 chrome.storage.local.get(['isOn'], function (result) {
-    if (result.isOn === undefined) {
-        chrome.storage.local.set({ isOn: true });
-    }
-});
+    if (result.isOn === undefined)
+        chrome.storage.local.set({
+            isOn: true
+        })
+})
 
 function setState(state) {
     if (state) document.body.classList.add('unminus')
     else document.body.classList.remove('unminus')
 }
 
+function yeet() {
+    document.querySelectorAll('.fMu6u').forEach(function (image) {
+        image.closest('figure[itemprop="image"]').classList.add('unsplashPlus')
+    })
+}
+
 // ajout .unminus au body si activé
 chrome.storage.local.get(['isOn'], function (result) {
     setState(result.isOn)
-});
+})
 
 // ajout .unminus au body à chaque changement de .isOn
 chrome.storage.onChanged.addListener(function (changes) {
-    for (var key in changes) {
-        setState(changes.isOn.newValue)
-    }
-});
+    setState(changes.isOn.newValue)
+})
 
 // adds .unsplashPlus when using Firefox since it can't handle :has() 🙄
-if ((navigator.userAgent.indexOf("Firefox") != -1)) {
-    window.addEventListener('scroll', () => {
-        document.querySelectorAll('.fMu6u').forEach(function (image) {
-            image.closest('figure[itemprop="image"]').classList.add('unsplashPlus')
-        });
-    });
-}
+if ((navigator.userAgent.indexOf("Firefox") != -1))
+    setInterval(yeet, 300)
